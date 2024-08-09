d37aeaa6afe10655b89abfd17a91ef8a4c51d9df
gvshacokk <gvst.wave@hotmail.com>
2024-08-08 21:26:45 -0300

    I JUST HAD A REALIZATION: Less is more
    I was overcomplicating the code, but now, with some friends help, i redid my entire code from scratch and it look so much better now. Less fuctions, but with the same function(lmao).
    Eu estava complicando demais o código, mas agora, com a ajuda de alguns amigos, refiz todo o meu código do zero e ele está muito melhor. Menos funções, mas com a mesma função (lmao).

--- main.cpp
+++ main.cpp
@@ -1,84 +1,61 @@
 #include <iostream>
 #define size 200
 
-using std::cout, std::endl, std::cin;
+using std::cout, std::endl, std::cin, std::end, std::begin;
 
-int numtt, cpf[size], menu;
+int cpf[size], saldo[size], menu, op;
 char nome[size][50], senha[size][50]; 
-static int linha = 0;
+static int linha;
 
 
 
 void menu1(){
-    cout << "Voce é:\n";
-    cout << "1- Usuario novo\n";
-    cout << "2- Usuario já cadastrado\n";
+    
 }
 
-void menu2(){
-    cout << "O que deseja fazer agora?\n";
-    cout << "1- Fazer deposito\n";
-    cout << "2- Realizar saque\n";
-    cout << "3- Retirar extrato?\n";
-    cout << "4- Sair\n";
-    cin  >> menu;
-}
-
-bool puxanum(){
-if (numtt == 1){
-    cout << "Seja vamos iniciar o seu cadastro agora mesmo!!\n";
-    return false;
-}
-if (numtt == 2){
-    cout << "Seja bem vindo de volta!\n";
-    return false;
-}
-else
-    cout << "Por favor insira um valor valido\n";
-    return true;
-}
-
-bool usernew(){
-     if (numtt == 1){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
 void comeco(){
+    do{
     cout << "Insira os dados requeridos abaixo:\n";
-if (usernew()){
     cout << "Seu nome(por favor insira apenas um nome):\n";
     cin >> nome[linha];
     cout << "Digite sua senha:\n";
     cin >> senha[linha];
-    linha++;
-    menu2();
-    }
-else{
     cout << "Insira seu cpf:\n";
     cin >> cpf[linha];
-    cout << "Insira sua senha:\n";
-    cin >> senha[linha];
-    }    
-}
-
-void cadastro(){
-
-}
+    cout << "Usuario cadastrado com sucesso\n";
+    saldo[linha] = 0;
+    linha++;
+    cout << "O que deseja fazer agora?\n 1- Continuar cadastrando 2- Sair \n";
+    cin >> op;
+    } while (op == 1);
+    }
 
 int main()
 {
 cout << "Bem vindo ao banco XXXX\n";
-menu1();
-cin >> numtt;
-
-while (puxanum()) {
-    menu1();
-    cin >> numtt;
-}
+cout << "Voce deseja:\n";
+cout << "1- Cadastrar um novo usuario\n";
+cout << "2- Pesquisar Usuario cadastrado\n";
+cout << "3- Fazer deposito\n";
+cout << "4- Realizar saque\n";
+cout << "5- Retirar extrato\n";
+cout << "6- Sair\n";
+cin >> menu;
+switch(menu){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        break;
+    }
 comeco();
 
 }
